57e31d318606130157539c64743c3c066945d08a
RCoralie <rachex.coralie@gmail.com>
2020-07-10 11:55:39 +0200

    [DEV][REGISTRATION] Add a method to visually compare registration methods

--- src/test_registration.cpp
+++ src/test_registration.cpp
@@ -165,6 +165,143 @@ void compareShiftRegistrationSpeed(const Mat &ref_img, const Mat &sensed_img) {
 }
 
 //----------------------------------------------------------------------------//
+void compareShiftRegistrationVisually(const Mat &ref_img, const Mat &sensed_img) {
+
+  // Display reference image
+  namedWindow("Reference image", WINDOW_GUI_NORMAL);
+  resizeWindow("Reference image", 300, 300);
+  moveWindow("Reference image", 0, 10);
+  imshow("Reference image", ref_img);
+
+  // Display sensed image
+  namedWindow("Sensed image", WINDOW_GUI_NORMAL);
+  resizeWindow("Sensed image", 300, 300);
+  moveWindow("Sensed image", 0, 400);
+  imshow("Sensed image", sensed_img);
+
+  waitKey(0);
+
+  // Display results
+
+  clock_t t0;
+  double duration;
+
+  // enhancedCorrelationCoefficientMaximization
+  t0 = clock();
+  Mat corr_euclidean = enhancedCorrelationCoefficientMaximization(ref_img, sensed_img, MOTION_EUCLIDEAN);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat corr_euclidean_reg;
+  warpAffine(sensed_img, corr_euclidean_reg, corr_euclidean, ref_img.size());
+  namedWindow("ECC (EUCLIDEAN MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("ECC (EUCLIDEAN MODEL)", 300, 300);
+  moveWindow("ECC (EUCLIDEAN MODEL)", 350, 10);
+  imshow("ECC (EUCLIDEAN MODEL)", corr_euclidean_reg);
+  cout << "ECC (EUCLIDEAN MODEL): -----------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(corr_euclidean) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  t0 = clock();
+  Mat corr_translation = enhancedCorrelationCoefficientMaximization(ref_img, sensed_img, MOTION_TRANSLATION);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat corr_translation_reg;
+  warpAffine(sensed_img, corr_translation_reg, corr_translation, ref_img.size());
+  namedWindow("ECC (TRANSLATION MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("ECC (TRANSLATION MODEL)", 300, 300);
+  moveWindow("ECC (TRANSLATION MODEL)", 350, 400);
+  imshow("ECC (TRANSLATION MODEL)", corr_translation_reg);
+  cout << "ECC (TRANSLATION MODEL): ---------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(corr_translation) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  waitKey(0);
+
+  // Features features methods
+  FBConfig config_orb_affine;
+  config_orb_affine.model = FBConfig::AFFINE;
+  config_orb_affine.detectorDescriptor = FBConfig::ORB_ALGO;
+  t0 = clock();
+  Mat orb_affine = featuresBasedMethod(ref_img, sensed_img, config_orb_affine);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat orb_affine_reg;
+  warpAffine(sensed_img, orb_affine_reg, orb_affine, ref_img.size());
+  namedWindow("ORB (AFFINE MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("ORB (AFFINE MODEL)", 300, 300);
+  moveWindow("ORB (AFFINE MODEL)", 650, 10);
+  imshow("ORB (AFFINE MODEL)", orb_affine_reg);
+  cout << "ORB (AFFINE MODEL): --------------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(orb_affine) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  FBConfig config_orb_rigid;
+  config_orb_rigid.model = FBConfig::AFFINE_PARTIAL;
+  config_orb_rigid.detectorDescriptor = FBConfig::ORB_ALGO;
+  t0 = clock();
+  Mat orb_rigid = featuresBasedMethod(ref_img, sensed_img, config_orb_rigid);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat orb_rigid_reg;
+  warpAffine(sensed_img, orb_rigid_reg, orb_rigid, ref_img.size());
+  namedWindow("ORB (RIGID MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("ORB (RIGID MODEL)", 300, 300);
+  moveWindow("ORB (RIGID MODEL)", 650, 400);
+  imshow("ORB (RIGID MODEL)", orb_rigid_reg);
+  cout << "ORB (RIGID MODEL): ---------------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(orb_rigid) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  waitKey(0);
+
+  FBConfig config_akaze_affine;
+  config_akaze_affine.model = FBConfig::AFFINE;
+  config_akaze_affine.detectorDescriptor = FBConfig::AKAZE_ALGO;
+  t0 = clock();
+  Mat akaze_affine = featuresBasedMethod(ref_img, sensed_img, config_akaze_affine);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat akaze_affine_reg;
+  warpAffine(sensed_img, akaze_affine_reg, akaze_affine, ref_img.size());
+  namedWindow("AKAZE (AFFINE MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("AKAZE (AFFINE MODEL)", 300, 300);
+  moveWindow("AKAZE (AFFINE MODEL)", 950, 10);
+  imshow("AKAZE (AFFINE MODEL)", akaze_affine_reg);
+  cout << "AKAZE (AFFINE MODEL): ------------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(akaze_affine) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  FBConfig config_akaze_rigid;
+  config_akaze_rigid.model = FBConfig::AFFINE_PARTIAL;
+  config_akaze_rigid.detectorDescriptor = FBConfig::AKAZE_ALGO;
+  t0 = clock();
+  Mat akaze_rigid = featuresBasedMethod(ref_img, sensed_img, config_akaze_rigid);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat akaze_rigid_reg;
+  warpAffine(sensed_img, akaze_rigid_reg, akaze_rigid, ref_img.size());
+  namedWindow("AKAZE (RIGID MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("AKAZE (RIGID MODEL)", 300, 300);
+  moveWindow("AKAZE (RIGID MODEL)", 950, 400);
+  imshow("AKAZE (RIGID MODEL)", akaze_rigid_reg);
+  cout << "AKAZE (RIGID MODEL): -------------------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(akaze_rigid) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  waitKey(0);
+
+  // Fourier-mellin transformation
+  t0 = clock();
+  Mat fmt = fourierMellinTransform(ref_img, sensed_img);
+  duration = (clock() - t0) / (double)CLOCKS_PER_SEC;
+  Mat fmt_reg;
+  warpAffine(sensed_img, fmt_reg, fmt, ref_img.size());
+  namedWindow("FOURIER-MELLIN (AFFINE MODEL)", WINDOW_GUI_NORMAL);
+  resizeWindow("FOURIER-MELLIN (AFFINE MODEL)", 300, 300);
+  moveWindow("FOURIER-MELLIN (AFFINE MODEL)", 1250, 10);
+  imshow("FOURIER-MELLIN (AFFINE MODEL)", fmt_reg);
+  cout << "FOURIER-MELLIN (AFFINE MODEL): ---------------------------" << endl;
+  cout << "t[x, y] : " << extractTranslationFromAffine(fmt) << endl;
+  cout << "execution time : " << duration << " secondes" << endl;
+
+  waitKey(0);
+}
+
+//----------------------------------------------------------------------------//
 int main(int argc, char **argv) {
   if (argc < 2) {
     cout << "need a reference image." << endl;
